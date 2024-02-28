fde205f1a3322635660cf83a192015cb21cf3dfd
kiransamjhana <kiranmanandhar956@gmail.com>
2024-02-28 20:27:12 +1100

    havent fineshed updated DB for delivery order

--- src/router/orderRouter.js
+++ src/router/orderRouter.js
@@ -4,6 +4,8 @@ import {
   getOrderById,
   getOrders,
   insertOrder,
+  updateOrder,
+  updateOrderById,
 } from "../model/Order/order.js";
 const router = express.Router();
 
@@ -30,6 +32,37 @@ router.get("/edit/:_id?", async (req, res, next) => {
       status: "success",
       message: "Here is the selected order",
       result,
+    });
+  } catch (error) {
+    `                      `;
+    next(error);
+  }
+});
+
+router.put("/update/:_id", async (req, res, next) => {
+  try {
+    const { _id } = req.params;
+    const { newStat, orderStatus } = req.body;
+
+    // Assuming you're sending _id and newStatus in the request body
+
+    // Perform the updateOrderStatus Axios call
+    console.log("Received _id:", _id, "orderStatus:", newStat, orderStatus);
+    const updateResult = await updateOrderById({ _id, orderStatus: newStat });
+
+    // Check if the update was successful
+    if (updateResult?._id) {
+      return res.json({
+        status: "success",
+        message: "The order status has been updated",
+        updateResult,
+      });
+    }
+
+    // Handle the case where the update was not successful
+    res.json({
+      status: "error",
+      message: "Failed to update order status. Please try again.",
     });
   } catch (error) {
     next(error);
